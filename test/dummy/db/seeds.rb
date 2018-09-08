46b112535135c69e5c01392fd557dbb5fb06ff64
Justin Malčić <j.malcic@me.com>
2018-09-08 10:54:36 +0100

    Add seeds

---
+++ test/dummy/db/seeds.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Spina
+  Account.first_or_create name: 'Collect', theme: 'conference'
+  User.first_or_create name: 'Justin', email: 'j.malcic@me.com',
+                       password: 'password', admin: true
+  module Collect
+    Institution.create name: 'University of Cambridge', city: 'Cambridge'
+    Institution.create name: 'University of Edinburgh', city: 'Edinburgh'
+    Room.create institution_id: 1, building: 'Lecture block', number: '2'
+    Room.create institution_id: 1, building: 'Lecture block', number: '3'
+    Room.create institution_id: 2, building: 'Medical school', number: 'G.14'
+    Room.create institution_id: 2, building: 'Medical school', number: 'G.152'
+    Room.create institution_id: 2, building: 'Medical school', number: 'G.16'
+    Conference.create institution_id: 1, start_date: '2017-04-07',
+                      finish_date: '2017-04-09'
+    Conference.create institution_id: 2, start_date: '2018-04-09',
+                      finish_date: '2018-04-11'
+    PresentationType.create conference_id: 1, name: 'Plenary', minutes: 80
+    PresentationType.create conference_id: 1, name: 'Poster', minutes: 30
+    PresentationType.create conference_id: 1, name: 'Oral', minutes: 20
+    PresentationType.create conference_id: 2, name: 'Plenary', minutes: 80
+    PresentationType.create conference_id: 2, name: 'Poster', minutes: 30
+    PresentationType.create conference_id: 2, name: 'Oral', minutes: 20
+    DietaryRequirement.create name: 'Pescetarian'
+    Delegate.create first_name: 'Justin', last_name: 'Malčić', institution_id: 1,
+                    email_address: 'j.malcic@me.com', conference_ids: [1, 2],
+                    dietary_requirement_ids: 1
+    Presentation.create conference_id: 1, presentation_type_id: 1,
+                        title: 'The Asymmetry and Antisymmetry of Syntax',
+                        room_id: 2, date: '2017-04-07', start_time: '10:00',
+                        abstract: 'Lorem ipsum', delegate_ids: [1]
+  end
+end
