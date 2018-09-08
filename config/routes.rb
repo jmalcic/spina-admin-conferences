b05903af3e6744332e265892d758df2c273a8297
Justin Malčić <j.malcic@me.com>
2018-09-08 10:49:37 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Spina::Engine.routes.draw do
+  namespace :collect do
+    resources :conferences, only: [:index, :show]
+    resources :presentations, only: [:index, :show]
+  end
   namespace :admin, path: Spina.config.backend_path do
-    resources :conferences
-    resources :delegates, except: [:show]
-    resources :presentations, except: [:show]
-    resources :presentation_types, except: [:show]
-    resources :dietary_requirements, except: [:show]
+    namespace :collect do
+      resources :conferences
+      resources :institutions, except: [:show]
+      resources :rooms, except: [:show]
+      resources :delegates, except: [:show]
+      resources :presentations, except: [:show]
+      resources :presentation_types, except: [:show]
+      resources :dietary_requirements, except: [:show]
+    end
   end
 end
