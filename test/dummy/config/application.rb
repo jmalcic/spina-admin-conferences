a55e005fdb8755be41dadbc29279575d2a9a7172
Justin Malčić <j.malcic@me.com>
2018-09-14 18:46:11 +0100

    Single-quote string

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,7 +3,7 @@ require_relative 'boot'
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require "spina/collect"
+require 'spina/collect'
 
 module Dummy
   class Application < Rails::Application
