d0c2e572bd57ada28f48a0459d8f49cddd67d9bf
Justin Malčić <j.malcic@me.com>
2018-09-14 18:54:41 +0100

    Add associations

--- app/models/spina/collect/conference.rb
+++ app/models/spina/collect/conference.rb
@@ -21,6 +21,9 @@ module Spina
                               foreign_key: :spina_collect_conference_id,
                               association_foreign_key:
                                   :spina_collect_delegate_id
+      has_one :conference_page_part, as: :conference_page_partable,
+              dependent: :destroy
+      has_one :conference_page, through: :conference_page_part
 
       validates_presence_of :start_date, :finish_date
       validates :finish_date, finish_date: true, unless: (proc do |a|
