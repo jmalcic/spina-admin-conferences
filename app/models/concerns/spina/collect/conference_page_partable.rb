b8e85b9d9207e1bdb159a5e08b1d7637728ac889
Justin Malčić <j.malcic@me.com>
2018-09-18 14:57:46 +0100

    Move partable logic to concern

--- app/models/concerns/spina/collect/conference_page_partable.rb
+++ app/models/concerns/spina/collect/conference_page_partable.rb
@@ -11,10 +11,52 @@ module Spina
       extend ActiveSupport::Concern
 
       included do
-        before_validation(on: :create) { build_conference_page_part }
+        before_validation :set_up_resource, :set_up_conference_page, on: :create
+        after_destroy { conference_page_part&.destroy }
 
-        has_one :conference_page_part, as: :conference_page_partable,
-                                       dependent: :destroy
+        has_one :conference_page_part, as: :conference_page_partable
+        has_one :conference_page, through: :conference_page_part
+
+        private
+
+        def page_partable_parameter
+          self.class.name.demodulize.parameterize
+        end
+
+        def resource
+          page_partable_parameter.pluralize
+        end
+
+        def set_up_resource
+          Spina::Resource.find_or_create_by(name: resource) do |resource|
+            resource.label = resource.name.titleize
+            resource.view_template = page_partable_parameter
+            if defined? self.class.parent_page
+              resource.parent_page_id = self.class.parent_page.id
+            end
+          end
+        end
+
+        def set_up_conference_page
+          @conference_page = Spina::Collect::ConferencePage.new(
+            title: name,
+            view_template:
+              Spina::Resource.find_by(name: resource).view_template,
+            deletable: false,
+            resource: Spina::Resource.find_by(name: resource),
+            parent_id: set_ancestry
+          )
+          set_ancestry
+          self.conference_page = @conference_page
+        end
+
+        def set_ancestry
+          if defined? parent_page
+            parent_page.id
+          else
+            Spina::Resource.find_by(name: resource).parent_page_id
+          end
+        end
       end
     end
   end
--- app/models/spina/collect/conference_page_part.rb
+++ app/models/spina/collect/conference_page_part.rb
@@ -9,56 +9,11 @@ module Spina
     # itself before it gets created, using the `#name` method of
     # `:conference_page_partable`.
     class ConferencePagePart < ApplicationRecord
-      before_validation :set_up_resource, :set_up_conference_page, on: :create
-
-      after_destroy do
-        conference_page_partable&.destroy if conference_page_partable
-      end
-
+      belongs_to :conference_page_partable, polymorphic: true,
+                                            inverse_of: :conference_page_part,
+                                            dependent: :destroy
       belongs_to :conference_page, inverse_of: :conference_page_part,
                                    dependent: :destroy
-      belongs_to :conference_page_partable, polymorphic: true,
-                                            inverse_of: :conference_page_part
-
-      private
-
-      def page_partable_parameter
-        conference_page_partable.class.name.demodulize.parameterize
-      end
-
-      def resource
-        page_partable_parameter.pluralize
-      end
-
-      def set_up_resource
-        Spina::Resource.find_or_create_by(name: resource) do |resource|
-          resource.label = resource.name.titleize
-          resource.view_template = page_partable_parameter
-          if defined? conference_page_partable.class.parent_page
-            resource.parent_page_id =
-              conference_page_partable.class.parent_page.id
-          end
-        end
-      end
-
-      def set_up_conference_page
-        build_conference_page(
-          title: conference_page_partable.name,
-          view_template: Spina::Resource.find_by(name: resource).view_template,
-          deletable: false,
-          resource: Spina::Resource.find_by(name: resource)
-        )
-        set_ancestry
-      end
-
-      def set_ancestry
-        conference_page.parent_id =
-          if defined? conference_page_partable.parent_page
-            conference_page_partable.parent_page.id
-          else
-            Spina::Resource.find_by(name: resource).parent_page_id
-          end
-      end
     end
   end
 end
