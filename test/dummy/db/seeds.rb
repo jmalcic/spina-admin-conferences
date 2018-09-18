91663421fc8ac44f0048165c84db8f0c0c6f7d9a
Justin Malčić <j.malcic@me.com>
2018-09-18 15:01:36 +0100

    Update seeds

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -9,6 +9,7 @@ module Spina
     Institution.create name: 'University of Edinburgh', city: 'Edinburgh'
     Room.create institution_id: 1, building: 'Lecture block', number: '2'
     Room.create institution_id: 1, building: 'Lecture block', number: '3'
+    Room.create institution_id: 1, building: 'Lecture block', number: 'entrance'
     Room.create institution_id: 2, building: 'Medical school', number: 'G.14'
     Room.create institution_id: 2, building: 'Medical school', number: 'G.152'
     Room.create institution_id: 2, building: 'Medical school', number: 'G.16'
@@ -16,19 +17,24 @@ module Spina
                       finish_date: '2017-04-09'
     Conference.create institution_id: 2, start_date: '2018-04-09',
                       finish_date: '2018-04-11'
-    PresentationType.create conference_id: 1, name: 'Plenary', minutes: 80
-    PresentationType.create conference_id: 1, name: 'Poster', minutes: 30
-    PresentationType.create conference_id: 1, name: 'Oral', minutes: 20
-    PresentationType.create conference_id: 2, name: 'Plenary', minutes: 80
-    PresentationType.create conference_id: 2, name: 'Poster', minutes: 30
-    PresentationType.create conference_id: 2, name: 'Oral', minutes: 20
+    PresentationType.create conference_id: 1, name: 'Plenary', minutes: 80,
+                            room_possession_ids: [2]
+    PresentationType.create conference_id: 1, name: 'Poster', minutes: 30,
+                            room_possession_ids: [3]
+    PresentationType.create conference_id: 1, name: 'Oral', minutes: 20,
+                            room_possession_ids: [1, 2]
+    PresentationType.create conference_id: 2, name: 'Plenary', minutes: 80,
+                            room_possession_ids: [5]
+    PresentationType.create conference_id: 2, name: 'Poster', minutes: 30,
+                            room_possession_ids: [4]
+    PresentationType.create conference_id: 2, name: 'Oral', minutes: 20,
+                            room_possession_ids: [5, 6]
     DietaryRequirement.create name: 'Pescetarian'
-    Delegate.create first_name: 'Justin', last_name: 'Malčić', institution_id: 1,
-                    email_address: 'j.malcic@me.com', conference_ids: [1, 2],
-                    dietary_requirement_ids: 1
-    Presentation.create conference_id: 1, presentation_type_id: 1,
-                        title: 'The Asymmetry and Antisymmetry of Syntax',
-                        room_id: 2, date: '2017-04-07', start_time: '10:00',
+    Delegate.create first_name: 'Justin', last_name: 'Malčić',
+                    institution_id: 1, email_address: 'j.malcic@me.com',
+                    conference_ids: [1, 2], dietary_requirement_ids: 1
+    Presentation.create title: 'The Asymmetry and Antisymmetry of Syntax',
+                        room_use_id: 3, date: '2017-04-07', start_time: '10:00',
                         abstract: 'Lorem ipsum', presenter_ids: [1]
   end
 end
