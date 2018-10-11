99d009e65097b3349e182f2a31ddab5b842641c2
Justin Malčić <j.malcic@me.com>
2018-10-11 21:48:17 +0100

    Namespace page parts

---
+++ app/models/spina/conferences/date.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Spina
+  module Conferences
+    # This class represents dates, without associated times.
+    class Date < ApplicationRecord
+      has_many :page_parts, as: :page_partable
+      has_many :layout_parts, as: :layout_partable
+      has_many :structure_parts, as: :structure_partable
+    end
+  end
+end
---
+++ app/models/spina/conferences/email_address.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Spina
+  module Conferences
+    # This class represents email addresses.
+    # The format is validated.
+    class EmailAddress < ApplicationRecord
+      has_many :page_parts, as: :page_partable
+      has_many :layout_parts, as: :layout_partable
+      has_many :structure_parts, as: :structure_partable
+
+      validates :content, email_address: true, unless: (proc do |a|
+        a.content.blank?
+      end)
+    end
+  end
+end
