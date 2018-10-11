c03a8da9d6f5a5859ef6e1361ff47e4251b9b651
Justin Malčić <j.malcic@me.com>
2018-10-11 21:47:48 +0100

    Create page part

---
+++ app/models/spina/conferences/time.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Spina
+  module Conferences
+    # This class represents times.
+    class Time < ApplicationRecord
+      has_many :page_parts, as: :page_partable
+      has_many :layout_parts, as: :layout_partable
+      has_many :structure_parts, as: :structure_partable
+
+      def date
+        content&.to_date
+      end
+      def date=(date)
+        @date = date
+      end
+      def time
+        content || nil
+      end
+      def time=(time)
+        @time = time
+        return unless @date
+        self.content = "#{@date} #{@time}".to_datetime.in_time_zone
+      end
+    end
+  end
+end
---
+++ db/migrate/20181009122503_create_spina_times.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateSpinaTimes < ActiveRecord::Migration[5.2] #:nodoc:
+  def change
+    create_table :spina_times do |t|
+      t.datetime :content
+
+      t.timestamps null: false
+    end
+  end
+end
