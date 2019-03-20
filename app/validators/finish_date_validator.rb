921fb0d3be0f5aafd124cacb1357b8412e07ce83
Justin Malčić <j.malcic@me.com>
2019-03-20 13:36:20 +0000

    Add guard clauses

--- app/validators/finish_date_validator.rb
+++ app/validators/finish_date_validator.rb
@@ -5,6 +5,7 @@
 class FinishDateValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value > record.start_date
+
     record.errors.add(attribute, options[:message] || :before_start_date)
   end
 end
