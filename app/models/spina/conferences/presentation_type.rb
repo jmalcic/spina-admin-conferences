cd9ae2e9dffa98040fbbbce44a5d21ea28cd7832
Justin Malčić <j.malcic@me.com>
2019-03-27 15:08:20 +0000

    Add nested associations

--- app/models/spina/conferences/presentation_type.rb
+++ app/models/spina/conferences/presentation_type.rb
@@ -9,6 +9,7 @@ module Spina
       belongs_to :conference, inverse_of: :presentation_types
       has_many :room_uses, dependent: :destroy
       has_many :room_possessions, through: :room_uses
+      has_many :rooms, through: :room_possessions
       has_many :presentations, through: :room_uses
 
       validates :name, :minutes, :room_uses, presence: true
