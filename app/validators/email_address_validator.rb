77859589fc6b13bc39840d35c76c0423c855f5b0
Justin Malčić <j.malcic@me.com>
2019-03-29 18:16:28 +0000

    Add delegate tests

--- app/validators/email_address_validator.rb
+++ app/validators/email_address_validator.rb
@@ -3,8 +3,6 @@
 # This class validates the format of the email address of an object.
 class EmailAddressValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    return if value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-
-    record.errors.add(attribute, options[:message] || :invalid_email_address)
+    record.errors.add(attribute, :invalid_email_address) unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   end
 end
--- test/models/spina/conferences/delegate_test.rb
+++ test/models/spina/conferences/delegate_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 module Spina
   module Conferences
     class DelegateTypeTest < ActiveSupport::TestCase
-      setup { @delegate = spina_conferences_delegates(:joe_bloggs) }
+      setup { @delegate = spina_conferences_delegates :joe_bloggs }
 
       test 'delegate attributes must not be empty' do
         delegate = Delegate.new
@@ -16,11 +16,22 @@ module Spina
       end
 
       test 'email address must be email address' do
+        assert @delegate.valid?
         assert_not @delegate.errors[:email_address].any?
-        invalid_delegate = Delegate.new(first_name: @delegate.first_name, last_name: @delegate.last_name,
-                                        conferences: @delegate.conferences, email_address: 'foo')
-        assert invalid_delegate.invalid?
-        assert invalid_delegate.errors[:email_address].any?
+        @delegate.email_address = 'foo'
+        assert @delegate.invalid?
+        assert @delegate.errors[:email_address].any?
+      end
+
+      test 'returns names' do
+        assert @delegate.respond_to? :full_name
+        assert @delegate.full_name.class == String
+        assert @delegate.respond_to? :full_name_and_institution
+        assert @delegate.full_name_and_institution.class == String
+        assert @delegate.respond_to? :reversed_name
+        assert @delegate.reversed_name.class == String
+        assert @delegate.respond_to? :reversed_name_and_institution
+        assert @delegate.reversed_name_and_institution.class == String
       end
     end
   end
