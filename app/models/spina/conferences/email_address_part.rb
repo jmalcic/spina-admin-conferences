3e9a60a221b103d8f56315a8ce1105e7a0bd049a
Justin Malčić <j.malcic@me.com>
2019-03-30 18:48:06 +0000

    Add test for email address validation

--- app/models/spina/conferences/email_address_part.rb
+++ app/models/spina/conferences/email_address_part.rb
@@ -8,7 +8,7 @@ module Spina
       has_many :layout_parts, as: :layout_partable
       has_many :structure_parts, as: :structure_partable
 
-      validates :content, email_address: true, unless: proc { |a| a.content.blank? }
+      validates :content, email_address: true
     end
   end
 end
--- app/validators/email_address_validator.rb
+++ app/validators/email_address_validator.rb
@@ -2,7 +2,14 @@
 
 # This class validates the format of the email address of an object.
 class EmailAddressValidator < ActiveModel::EachValidator
+  def parse(value)
+    address = Mail::Address.new(value)
+    address.domain.present? && address.local.present?
+  rescue Mail::Field::IncompleteParseError
+    false
+  end
+
   def validate_each(record, attribute, value)
-    record.errors.add(attribute, :invalid_email_address) unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    record.errors.add(attribute, :invalid_email_address) unless value.blank? || parse(value)
   end
 end
