005399c2a6807682d473b1b636588059df4b203d
Justin Malčić <j.malcic@me.com>
2019-03-30 21:26:15 +0000

    Refactor

--- test/models/spina/conferences/conference_page_part_test.rb
+++ test/models/spina/conferences/conference_page_part_test.rb
@@ -2,10 +2,12 @@
 
 require 'test_helper'
 
-module Spina::Conferences
-  class ConferencePagePartTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+module Spina
+  module Conferences
+    class ConferencePagePartTest < ActiveSupport::TestCase
+      # test "the truth" do
+      #   assert true
+      # end
+    end
   end
 end
--- test/models/spina/conferences/conference_page_test.rb
+++ test/models/spina/conferences/conference_page_test.rb
@@ -2,10 +2,12 @@
 
 require 'test_helper'
 
-module Spina::Conferences
-  class ConferencePageTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+module Spina
+  module Conferences
+    class ConferencePageTest < ActiveSupport::TestCase
+      # test "the truth" do
+      #   assert true
+      # end
+    end
   end
 end
