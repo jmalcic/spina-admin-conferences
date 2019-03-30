39d495a0c127f90496aad21c6fbe7375c2d4c763
Justin Malčić <j.malcic@me.com>
2019-03-30 21:26:05 +0000

    Test time validation

--- test/models/spina/conferences/time_part_test.rb
+++ test/models/spina/conferences/time_part_test.rb
@@ -2,8 +2,15 @@
 
 require 'test_helper'
 
-class Spina::TimePartTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+module Spina
+  class TimePartTypeTest < ActiveSupport::TestCase
+    setup { @time_part = spina_conferences_time_parts :valid_time }
+
+    test 'content must be a time' do
+      assert @time_part.valid?
+      assert_not @time_part.errors[:content].any?
+      @time_part.content = 'invalid'
+      assert @time_part.content.nil?
+    end
+  end
 end
