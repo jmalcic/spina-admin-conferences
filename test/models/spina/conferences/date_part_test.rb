aef448a5c6e93a9468e7a8ebd0fdeec40e2dab02
Justin Malčić <j.malcic@me.com>
2019-03-30 19:00:20 +0000

    Refactor

--- test/models/spina/conferences/date_part_test.rb
+++ test/models/spina/conferences/date_part_test.rb
@@ -2,8 +2,10 @@
 
 require 'test_helper'
 
-class Spina::DatePartTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+module Spina
+  class DatePartTypeTest < ActiveSupport::TestCase
+    # test "the truth" do
+    #   assert true
+    # end
+  end
 end
