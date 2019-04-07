4fafc86e3c58432bcd97dfe177143042b85ac438
Justin Malčić <j.malcic@me.com>
2019-04-07 17:58:27 +0200

    Improve style

--- test/spina/conferences_test.rb
+++ test/spina/conferences_test.rb
@@ -1,7 +1,13 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
-class Spina::Conferences::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Spina::Conferences
+module Spina
+  module Conferences
+    class Test < ActiveSupport::TestCase
+      test 'truth' do
+        assert_kind_of Module, Spina::Conferences
+      end
+    end
   end
 end
