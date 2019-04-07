36151d290330a7a2cf181b490827d9bf167fb8f9
Justin Malčić <j.malcic@me.com>
2019-04-07 17:25:21 +0200

    Correct module

--- test/controllers/spina/admin/presentations_controller_test.rb
+++ test/controllers/spina/admin/presentations_controller_test.rb
@@ -4,52 +4,54 @@ require 'test_helper'
 
 module Spina
   module Admin
-    class PresentationsControllerTest < ActionDispatch::IntegrationTest
-      include Engine.routes.url_helpers
-      include ::Spina::Conferences
+    module Conferences
+      class PresentationsControllerTest < ActionDispatch::IntegrationTest
+        include Engine.routes.url_helpers
+        include ::Spina::Conferences
 
-      setup do
-        @presentation = spina_conferences_presentations :asymmetry_and_antisymmetry
-        @user = spina_users :joe
-        post admin_sessions_url, params: { email: @user.email, password: 'password' }
-      end
-
-      test 'should get index' do
-        get admin_conferences_presentations_url
-        assert_response :success
-      end
-
-      test 'should get new' do
-        get new_admin_conferences_presentation_url
-        assert_response :success
-      end
-
-      test 'should create presentation' do
-        assert_difference(%w[Presentation.count ConferencePagePart.count ConferencePage.count]) do
-          attributes = @presentation.attributes
-          attributes[:presenter_ids] = @presentation.presenters.collect(&:id)
-          post admin_conferences_presentations_url, params: { presentation: attributes }
+        setup do
+          @presentation = spina_conferences_presentations :asymmetry_and_antisymmetry
+          @user = spina_users :joe
+          post admin_sessions_url, params: { email: @user.email, password: 'password' }
         end
 
-        assert_redirected_to admin_conferences_presentations_url
-      end
-
-      test 'should get edit' do
-        get edit_admin_conferences_presentation_url(@presentation)
-        assert_response :success
-      end
-
-      test 'should update presentation' do
-        patch admin_conferences_presentation_url(@presentation), params: { presentation: @presentation.attributes }
-        assert_redirected_to admin_conferences_presentations_url
-      end
-
-      test 'should destroy presentation' do
-        assert_difference(%w[Presentation.count ConferencePagePart.count ConferencePage.count], -1) do
-          delete admin_conferences_presentation_url(@presentation)
+        test 'should get index' do
+          get admin_conferences_presentations_url
+          assert_response :success
         end
 
-        assert_redirected_to admin_conferences_presentations_url
+        test 'should get new' do
+          get new_admin_conferences_presentation_url
+          assert_response :success
+        end
+
+        test 'should create presentation' do
+          assert_difference(%w[Presentation.count ConferencePagePart.count ConferencePage.count]) do
+            attributes = @presentation.attributes
+            attributes[:presenter_ids] = @presentation.presenters.collect(&:id)
+            post admin_conferences_presentations_url, params: { presentation: attributes }
+          end
+
+          assert_redirected_to admin_conferences_presentations_url
+        end
+
+        test 'should get edit' do
+          get edit_admin_conferences_presentation_url(@presentation)
+          assert_response :success
+        end
+
+        test 'should update presentation' do
+          patch admin_conferences_presentation_url(@presentation), params: { presentation: @presentation.attributes }
+          assert_redirected_to admin_conferences_presentations_url
+        end
+
+        test 'should destroy presentation' do
+          assert_difference(%w[Presentation.count ConferencePagePart.count ConferencePage.count], -1) do
+            delete admin_conferences_presentation_url(@presentation)
+          end
+
+          assert_redirected_to admin_conferences_presentations_url
+        end
       end
     end
   end
--- test/controllers/spina/admin/rooms_controller_test.rb
+++ test/controllers/spina/admin/rooms_controller_test.rb
@@ -4,48 +4,50 @@ require 'test_helper'
 
 module Spina
   module Admin
-    class RoomsControllerTest < ActionDispatch::IntegrationTest
-      include Engine.routes.url_helpers
-      include ::Spina::Conferences
+    module Conferences
+      class RoomsControllerTest < ActionDispatch::IntegrationTest
+        include Engine.routes.url_helpers
+        include ::Spina::Conferences
 
-      setup do
-        @room = spina_conferences_rooms :lecture_block_2
-        @user = spina_users :joe
-        post admin_sessions_url, params: { email: @user.email, password: 'password' }
-      end
-
-      test 'should get index' do
-        get admin_conferences_rooms_url
-        assert_response :success
-      end
-
-      test 'should get new' do
-        get new_admin_conferences_room_url
-        assert_response :success
-      end
-
-      test 'should create room' do
-        assert_difference('Room.count') do
-          post admin_conferences_rooms_url, params: { room: @room.attributes }
+        setup do
+          @room = spina_conferences_rooms :lecture_block_2
+          @user = spina_users :joe
+          post admin_sessions_url, params: { email: @user.email, password: 'password' }
         end
 
-        assert_redirected_to admin_conferences_rooms_url
-      end
+        test 'should get index' do
+          get admin_conferences_rooms_url
+          assert_response :success
+        end
 
-      test 'should get edit' do
-        get edit_admin_conferences_room_url(@room)
-        assert_response :success
-      end
+        test 'should get new' do
+          get new_admin_conferences_room_url
+          assert_response :success
+        end
 
-      test 'should update room' do
-        patch admin_conferences_room_url(@room), params: { room: @room.attributes }
-        assert_redirected_to admin_conferences_rooms_url
-      end
+        test 'should create room' do
+          assert_difference('Room.count') do
+            post admin_conferences_rooms_url, params: { room: @room.attributes }
+          end
 
-      test 'should destroy room' do
-        assert_difference('Room.count', -1) { delete admin_conferences_room_url(@room) }
+          assert_redirected_to admin_conferences_rooms_url
+        end
 
-        assert_redirected_to admin_conferences_rooms_url
+        test 'should get edit' do
+          get edit_admin_conferences_room_url(@room)
+          assert_response :success
+        end
+
+        test 'should update room' do
+          patch admin_conferences_room_url(@room), params: { room: @room.attributes }
+          assert_redirected_to admin_conferences_rooms_url
+        end
+
+        test 'should destroy room' do
+          assert_difference('Room.count', -1) { delete admin_conferences_room_url(@room) }
+
+          assert_redirected_to admin_conferences_rooms_url
+        end
       end
     end
   end
