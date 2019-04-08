31bc980854b2e38cd691cfa4de1b291407fd0cf1
Justin Malčić <j.malcic@me.com>
2019-04-08 16:36:10 +0200

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'resque/server'
-
 Spina::Engine.routes.draw do
   namespace :admin, path: Spina.config.backend_path do
     resources :conference_pages, controller: 'conferences/conference_pages'
@@ -30,7 +28,6 @@ Spina::Engine.routes.draw do
       resources :dietary_requirements, except: [:show] do
         post :import, on: :collection
       end
-      mount Resque::Server, at: '/jobs'
     end
   end
 end
