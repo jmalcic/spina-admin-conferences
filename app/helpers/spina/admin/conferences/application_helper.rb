5ae06d40f34b7172ca4bd53bda05e4500fd42b85
Justin Malčić <j.malcic@me.com>
2019-06-07 12:19:59 +0100

    Allow helpers to select webpackers

--- app/helpers/spina/admin/conferences/application_helper.rb
+++ app/helpers/spina/admin/conferences/application_helper.rb
@@ -8,8 +8,24 @@ module Spina
       module ApplicationHelper #:nodoc:
         include ::Webpacker::Helper
 
+        helper_methods = ::Webpacker::Helper.public_instance_methods false
+        helper_methods.delete :current_webpacker_instance
+        helper_methods.each do |method|
+          alias_method :"base_#{method}", :"#{method}"
+          redefine_method method do |*args, **options|
+            update_webpacker_instance options.delete(:instance)
+            send :"base_#{method}", *args, **options
+          end
+        end
+
+        private
+
+        def update_webpacker_instance(instance)
+          @webpacker = instance || Webpacker.instance
+        end
+
         def current_webpacker_instance
-          ::Spina::Conferences::Engine.webpacker
+          @webpacker || Webpacker.instance
         end
       end
     end
