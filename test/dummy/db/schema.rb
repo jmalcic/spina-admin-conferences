b4642359ec0433d40efa5294ff8e51d93fb69c42
Justin Malčić <j.malcic@me.com>
2019-06-22 19:49:00 +0100

    Migrate

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_131354) do
+ActiveRecord::Schema.define(version: 2019_06_22_141805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "spina_accounts", id: :serial, force: :cascade do |t|
     t.string "name"
@@ -113,6 +134,15 @@ ActiveRecord::Schema.define(version: 2019_04_08_131354) do
     t.datetime "updated_at", null: false
     t.bigint "logo_id"
     t.index ["logo_id"], name: "index_spina_conferences_institutions_on_logo_id"
+  end
+
+  create_table "spina_conferences_parts", force: :cascade do |t|
+    t.string "title"
+    t.string "name"
+    t.string "partable_type"
+    t.bigint "partable_id"
+    t.string "pageable_type"
+    t.bigint "pageable_id"
   end
 
   create_table "spina_conferences_presentation_types", force: :cascade do |t|
@@ -368,6 +398,7 @@ ActiveRecord::Schema.define(version: 2019_04_08_131354) do
     t.datetime "password_reset_sent_at"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "spina_conferences_conference_page_parts", "spina_pages", column: "conference_page_id", on_delete: :cascade
   add_foreign_key "spina_conferences_conferences", "spina_conferences_institutions", column: "institution_id", on_delete: :cascade
   add_foreign_key "spina_conferences_delegates", "spina_conferences_institutions", column: "institution_id", on_delete: :cascade
