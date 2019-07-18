22c0c1aa9cc62d1b45621e24319133c85498c349
Justin Malčić <j.malcic@me.com>
2019-07-18 23:58:10 +0200

    Add forgery protection

--- app/controllers/spina/conferences/application_controller.rb
+++ app/controllers/spina/conferences/application_controller.rb
@@ -4,6 +4,7 @@ module Spina
   module Conferences
     # Base class from which controllers inherit
     class ApplicationController < ::Spina::ApplicationController
+      protect_from_forgery with: :exception
     end
   end
 end
