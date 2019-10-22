27d317366d850e1f584c5257b1fe8d661a809694
Justin Malčić <j.malcic@me.com>
2019-10-22 13:33:13 -0400

    Fix manifests

--- app/assets/config/spina_conferences_manifest.js
+++ app/assets/config/spina_conferences_manifest.js
@@ -1,3 +1,10 @@
 //= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
+//= link_directory ../stylesheets/conference .css
+//= link_directory ../stylesheets/spina/admin/conferences .css
+//= link_directory ../javascripts/conference .js
+//= link_directory ../javascripts/spina/admin/conferences .js
+
+//= link spina/admin/scaffold.js
+
+//= link spina/admin/application.css
+//= link spina/admin/application.js
