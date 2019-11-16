9a853ee3b78540d6b5cf139ca06e53fdf536797d
Justin Malčić <j.malcic@me.com>
2019-11-15 20:20:49 -0500

    Correct config

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,6 +3,11 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include ::Spina::Engine.routes.url_helpers
+
+  def url_options
+    default_url_options.merge(host: Capybara.app_host)
+  end
+
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 800]
-  ActionDispatch.test_app = ::Spina::Engine
 end
