8e992e6bb9f8e3f68b81d8ab73a7887614e12d14
Justin Malčić <j.malcic@me.com>
2019-11-17 13:54:42 -0500

    Silence Puma when running system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,6 +2,10 @@
 
 require 'test_helper'
 
+ActiveSupport.on_load :action_dispatch_system_test_case do
+  ActionDispatch::SystemTesting::Server.silence_puma = true
+end
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include ::Spina::Engine.routes.url_helpers
 
