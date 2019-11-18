2fd55b5fe216fb0ce80d19fda41470fee3e7b4bf
Justin Malčić <j.malcic@me.com>
2019-11-17 20:32:49 -0500

    Add test

--- test/models/spina/conferences/date_part_test.rb
+++ test/models/spina/conferences/date_part_test.rb
@@ -3,9 +3,14 @@
 require 'test_helper'
 
 module Spina
-    # test "the truth" do
-    #   assert true
-    # end
   class DatePartTest < ActiveSupport::TestCase
+    setup { @date_part = spina_conferences_date_parts :valid_date }
+
+    test 'content must be a date' do
+      assert @date_part.valid?
+      assert_not @date_part.errors[:content].any?
+      @date_part.content = 'invalid'
+      assert @date_part.content.nil?
+    end
   end
 end
