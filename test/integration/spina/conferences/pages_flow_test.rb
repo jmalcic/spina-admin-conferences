63b5bd4a0434e93912c4966dfea8256d1d962d48
Justin Malčić <j.malcic@me.com>
2019-11-20 21:44:05 -0500

    Test blank pages

--- test/integration/spina/conferences/pages_flow_test.rb
+++ test/integration/spina/conferences/pages_flow_test.rb
@@ -7,26 +7,63 @@ module Spina
     class PagesFlowTest < ActionDispatch::IntegrationTest
       include ::Spina::Engine.routes.url_helpers
 
-      test 'can visit pages' do
+      test 'visit homepage' do
         get spina_pages(:homepage).materialized_path
         assert_response :success
-        get spina_pages(:information).materialized_path
+        get spina_pages(:empty_homepage).materialized_path
         assert_response :success
-        get spina_pages(:about).materialized_path
-        assert_response :success
-        get spina_pages(:blank).materialized_path
-        assert_response :success
-      end
-
-      test 'can visit pages in locale' do
         I18n.locale = :en
         get spina_pages(:homepage).materialized_path
         assert_response :success
+        get spina_pages(:empty_homepage).materialized_path
+        assert_response :success
+      end
+
+      test 'visit information page' do
         get spina_pages(:information).materialized_path
         assert_response :success
+        get spina_pages(:empty_information).materialized_path
+        assert_response :success
+        I18n.locale = :en
+        get spina_pages(:information).materialized_path
+        assert_response :success
+        get spina_pages(:empty_information).materialized_path
+        assert_response :success
+      end
+
+      test 'visit about page' do
         get spina_pages(:about).materialized_path
         assert_response :success
+        get spina_pages(:empty_about).materialized_path
+        assert_response :success
+        I18n.locale = :en
+        get spina_pages(:about).materialized_path
+        assert_response :success
+        get spina_pages(:empty_about).materialized_path
+        assert_response :success
+      end
+
+      test 'visit committee page' do
+        get spina_pages(:committee).materialized_path
+        assert_response :success
+        get spina_pages(:empty_committee).materialized_path
+        assert_response :success
+        I18n.locale = :en
+        get spina_pages(:committee).materialized_path
+        assert_response :success
+        get spina_pages(:empty_committee).materialized_path
+        assert_response :success
+      end
+
+      test 'visit blank page' do
         get spina_pages(:blank).materialized_path
+        assert_response :success
+        get spina_pages(:empty_blank).materialized_path
+        assert_response :success
+        I18n.locale = :en
+        get spina_pages(:blank).materialized_path
+        assert_response :success
+        get spina_pages(:empty_blank).materialized_path
         assert_response :success
       end
     end
