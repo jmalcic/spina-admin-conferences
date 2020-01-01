10cdce5ebab40ed3ee4c16cdbb7c96c8468c55ab
Justin Malčić <j.malcic@me.com>
2020-01-01 16:30:39 +0000

    Bump Ruby version

--- lib/spina/conferences.rb
+++ lib/spina/conferences.rb
@@ -7,9 +7,8 @@ require 'spina/conferences/partable_extensions'
 require 'spina/conferences/option_extensions'
 require 'spina/conferences/structure_extensions'
 require 'spina/admin/conferences/pages_helper_extensions'
-require 'icalendar'
-require 'octicons_helper'
 require 'rails-i18n'
+require 'icalendar'
 
 module Spina
   module Conferences #:nodoc:
