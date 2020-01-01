49bf2e4507ee4ff4bdd10e6eb1de9975414be264
Justin Malčić <j.malcic@me.com>
2020-01-01 17:13:00 +0000

    Update manifest

--- app/assets/config/spina_conferences_manifest.js
+++ app/assets/config/spina_conferences_manifest.js
@@ -1,6 +1,4 @@
 //= link_tree ../images
-//= link_directory ../stylesheets/conference .css
-//= link_directory ../javascripts/conference .js
 //= link spina/admin/conferences/application.js
 //= link spina/admin/conferences/application.css
 //= link spina_manifest.js
