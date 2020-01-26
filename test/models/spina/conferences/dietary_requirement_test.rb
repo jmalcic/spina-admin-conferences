cc48e51832c5e51ec3d37a0fba6589526ea3d3f9
Justin Malčić <j.malcic@me.com>
2020-01-25 23:06:05 -0500

    Remove redundant test setup

--- test/models/spina/conferences/dietary_requirement_test.rb
+++ test/models/spina/conferences/dietary_requirement_test.rb
@@ -5,8 +5,6 @@ require 'test_helper'
 module Spina
   module Conferences
     class DietaryRequirementTest < ActiveSupport::TestCase
-      setup { @room = spina_conferences_dietary_requirements :pescetarian }
-
       test 'dietary requirement attributes must not be empty' do
         dietary_requirement = DietaryRequirement.new
         assert dietary_requirement.invalid?
