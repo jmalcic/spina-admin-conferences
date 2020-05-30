9a13b4bde5574a04900ab4a6873dba5c9d266d4a
Justin Malčić <j.malcic@me.com>
2020-05-30 20:21:08 +0100

    Extend Mobility

--- app/models/spina/admin/conferences/application_record.rb
+++ app/models/spina/admin/conferences/application_record.rb
@@ -4,6 +4,8 @@ module Spina
   module Admin
     module Conferences
       class ApplicationRecord < ActiveRecord::Base #:nodoc:
+        extend Mobility
+
         self.abstract_class = true
       end
     end
