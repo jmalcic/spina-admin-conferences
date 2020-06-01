c328b1724ced557eb54c4a70bd92a75f1368e688
Justin Malčić <j.malcic@me.com>
2020-06-01 17:02:15 +0100

    Refactor jobs

--- app/jobs/spina/admin/conferences/delegate_import_job.rb
+++ app/jobs/spina/admin/conferences/delegate_import_job.rb
@@ -8,29 +8,16 @@ module Spina
         queue_as :default
 
         def perform(csv)
-          Delegate.transaction do
-            import(csv) do |row|
-              delegate = find_or_create_delegate(row)
-              unless delegate.new_record?
-                set_params delegate, row
-                delegate.save
-              end
+          import(csv) do |row|
+            Delegate.transaction do
+              institution = Institution.i18n.find_by(row[:institution])
+              dietary_requirements = DietaryRequirement.i18n.where(name: row[:dietary_requirements])
+              conferences = row[:conferences].collect { |params| Conference.i18n.find_by(params) }
+              Delegate.create! first_name: row[:first_name], last_name: row[:last_name],
+                               email_address: row[:email_address], institution: institution,
+                               dietary_requirements: dietary_requirements, conferences: conferences
             end
           end
-        end
-
-        def find_or_create_delegate(row)
-          Delegate.find_or_create_by! email_address: row[:email_address] do |new_delegate|
-            new_delegate.first_name = row[:first_name]
-            new_delegate.last_name = row[:last_name]
-            set_params new_delegate, row
-          end
-        end
-
-        def set_params(delegate, row)
-          delegate.institution = find_institution(row[:institution])
-          delegate.dietary_requirements = find_dietary_requirements(row[:dietary_requirements])
-          delegate.conferences.concat find_conferences(row[:conferences])
         end
       end
     end
--- app/jobs/spina/admin/conferences/import_job.rb
+++ app/jobs/spina/admin/conferences/import_job.rb
@@ -13,62 +13,9 @@ module Spina
           field
         end
 
-        def import(csv)
-          CSV.parse(csv, encoding: 'UTF-8', headers: true, header_converters: :symbol,
-                         converters: %i[date_time date json]).collect { |row| yield(row) }
-        end
-
-        def find_institution(params)
-          Institution.i18n.find_by! name: params[:name], city: params[:city]
-        end
-
-        def find_room(params)
-          Room.i18n.find_by! building: params[:building], number: params[:number],
-                             institution: find_institution(params[:institution])
-        end
-
-        def find_rooms(params)
-          params.collect(&method(:find_room))
-        end
-
-        def find_dietary_requirements(params)
-          params.collect do |dietary_requirement|
-            DietaryRequirement.i18n.find_by! name: dietary_requirement[:name]
-          end
-        end
-
-        def find_conference(params)
-          Conference.i18n.find_by! name: params[:name], dates: params[:start_date]..params[:finish_date]
-        end
-
-        def find_conferences(params)
-          params.collect(&method(:find_conference))
-        end
-
-        def find_presentation_type(params)
-          PresentationType.i18n.find_by! conference: find_conference(params[:conference]), name: params[:name]
-        end
-
-        def find_delegates(params)
-          params.collect do |presenter|
-            Delegate.find_by! first_name: presenter[:first_name], last_name: presenter[:last_name],
-                              institution: find_institution(presenter[:institution])
-          end
-        end
-
-        def find_session(params)
-          presentation_type_params = params[:presentation_type]
-          room_params = params[:room]
-          copy_value :conference, from: params, to: presentation_type_params
-          Session.find_by presentation_type: find_presentation_type(presentation_type_params),
-                          room: find_room(room_params)
-        end
-
-        def copy_value(value, from: nil, to: nil)
-          return unless from.present? && to.present?
-
-          source = from[value]
-          to.is_a?(Array) ? to.each { |element| element[value] = source } : to[value] = source
+        def import(file)
+          csv_rows = CSV.parse file, encoding: 'UTF-8', headers: true, header_converters: :symbol, converters: [:json]
+          csv_rows.collect { |row| yield(row) }
         end
       end
     end
