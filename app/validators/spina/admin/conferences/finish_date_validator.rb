5ff52a8f28585c1cadad10fb99d1a6509f0c9e26
Justin Malčić <j.malcic@me.com>
2020-06-08 22:09:42 +0100

    Update finish date validator

--- app/validators/spina/admin/conferences/finish_date_validator.rb
+++ app/validators/spina/admin/conferences/finish_date_validator.rb
@@ -2,14 +2,18 @@
 
 module Spina
   module Admin
-  module Conferences
-    # This class validates the finish date of an object to make sure it occurs
-    # after the start date.
-    class FinishDateValidator < ActiveModel::EachValidator
-      def validate_each(record, attribute, value)
-        record.errors.add(attribute, :before_start_date) unless value.blank? || value >= record.start_date
+    module Conferences
+      # This class validates the finish date of an object to make sure it occurs
+      # after the start date.
+      class FinishDateValidator < ActiveModel::EachValidator
+        def validate_each(record, attribute, value)
+          return if value.blank? || value >= record.start_date
+
+          record.errors.add(attribute, :before_start_date)
+        rescue ArgumentError
+          nil
+        end
       end
     end
-  end
   end
 end
--- test/validators/spina/admin/conferences/finish_date_validator_test.rb
+++ test/validators/spina/admin/conferences/finish_date_validator_test.rb
@@ -12,14 +12,14 @@ module Spina
         end
 
         test 'dates greater than or equal to the start date are valid' do
-          @conference.finish_date = @conference.start_date + 1.day
+          @conference.finish_date = (@conference.start_date + 1.day).iso8601
           assert_nil @validator.validate_each(@conference, :finish_date, @conference.finish_date)
-          @conference.finish_date = @conference.start_date
+          @conference.finish_date = @conference.start_date.iso8601
           assert_nil @validator.validate_each(@conference, :finish_date, @conference.finish_date)
         end
 
         test 'dates before the start date are invalid' do
-          @conference.finish_date = @conference.start_date - 1.day
+          @conference.finish_date = (@conference.start_date - 1.day).iso8601
           assert_includes @validator.validate_each(@conference, :finish_date, @conference.finish_date),
                           'is before start date'
         end
