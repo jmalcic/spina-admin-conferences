d537e33d2956d398190b1f05e0ce005a46aa5730
Justin Malčić <j.malcic@me.com>
2020-09-05 18:54:46 +0100

    Raise on unpermitted params

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -47,4 +47,6 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   config.action_view.raise_on_missing_translations = true
+
+  ActionController::Parameters.action_on_unpermitted_parameters = :raise
 end
