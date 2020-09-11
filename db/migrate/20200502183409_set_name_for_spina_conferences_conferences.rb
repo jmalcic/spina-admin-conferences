5f56de93c54f1626c25050e3d4aeef20e8893bd8
Justin Malčić <j.malcic@me.com>
2020-09-11 15:23:55 +0100

    Correct migration

--- db/migrate/20200502183409_set_name_for_spina_conferences_conferences.rb
+++ db/migrate/20200502183409_set_name_for_spina_conferences_conferences.rb
@@ -2,45 +2,46 @@
 
 class SetNameForSpinaConferencesConferences < ActiveRecord::Migration[6.0] # :nodoc:
   def up
-    insert <<-SQL.squish
-      INSERT INTO "spina_conferences_conference_translations" (
-        "locale", "spina_conferences_conference_id", "name", "created_at", "updated_at"
-      )
-        SELECT
-          '#{I18n.default_locale}' "locale",
-          "spina_conferences_conferences"."id" AS "spina_conferences_conference_id",
-          date_part('year', lower("spina_conferences_conferences"."dates")) AS "name",
-          "spina_conferences_conferences"."created_at",
-          "spina_conferences_conferences"."updated_at"
-        FROM "spina_conferences_conferences"
-    SQL
+    add_translations
   end
 
   def down
-    update <<-SQL.squish
-      WITH conference_to_instutition_map AS (
-        SELECT
-          "spina_conferences_conferences"."id" AS "conference_id",
-          MAX("spina_conferences_rooms"."institution_id") AS "institution_id"
-        FROM
-          "spina_conferences_conferences"
-        LEFT JOIN "spina_conferences_presentation_types" ON
-          "spina_conferences_conferences"."id" = "spina_conferences_presentation_types"."conference_id"
-        LEFT JOIN "spina_conferences_room_uses" ON
-          "spina_conferences_presentation_types"."id" = "spina_conferences_room_uses"."presentation_type_id"
-        LEFT JOIN "spina_conferences_room_possessions" ON
-          "spina_conferences_room_uses"."room_possession_id" = "spina_conferences_room_possessions"."id"
-        LEFT JOIN "spina_conferences_rooms" ON
-          "spina_conferences_room_possessions"."room_id" = "spina_conferences_rooms"."id"
-        GROUP BY
-          "spina_conferences_conferences"."id"
+    set_institution_from_room
+    delete_translations
+  end
+
+  private
+
+  def set_institution_from_room
+    exec_update <<-SQL.squish, 'set institution from room'
+      UPDATE spina_conferences_conferences SET (institution_id) = (
+        SELECT spina_conferences_rooms.institution_id
+          FROM spina_conferences_presentation_types
+            INNER JOIN spina_conferences_room_uses ON spina_conferences_presentation_types.id = presentation_type_id
+            INNER JOIN spina_conferences_room_possessions ON spina_conferences_room_possessions.id = room_possession_id
+            INNER JOIN spina_conferences_rooms ON spina_conferences_rooms.id = room_id
+          WHERE spina_conferences_conferences.id = spina_conferences_presentation_types.conference_id
+          ORDER BY room_id
+          LIMIT 1
       )
-      UPDATE "spina_conferences_conferences"
-        SET "institution_id" = "conference_to_instutition_map"."institution_id"
-      FROM
-        "spina_conferences_conferences" AS "base_table"
-      LEFT JOIN conference_to_instutition_map ON
-        "base_table"."id" = "conference_to_instutition_map"."conference_id"
+    SQL
+  end
+
+  def delete_translations
+    exec_delete <<-SQL.squish, 'delete translations'
+      DELETE FROM spina_conferences_conference_translations
+    SQL
+  end
+
+  def add_translations
+    binds = [ActiveRecord::Relation::QueryAttribute.new('locale', I18n.default_locale, ActiveRecord::Type::String.new)]
+    exec_insert <<-SQL.squish, 'add translations', binds
+      INSERT INTO spina_conferences_conference_translations (locale, spina_conferences_conference_id, "name", created_at, updated_at)
+        SELECT $1, spina_conferences_conferences.id,
+               concat_ws(' ', spina_conferences_institution_translations.name, date_part('year', lower(dates))),
+               spina_conferences_conferences.created_at, spina_conferences_conferences.updated_at
+          FROM spina_conferences_conferences
+            LEFT JOIN spina_conferences_institution_translations ON spina_conferences_institution_id = institution_id AND locale = $1
     SQL
   end
 end
