8f59e63bbb482eaac3275879db6e546fe481d9f1
codefactor-io <support@codefactor.io>
2020-10-04 11:24:30 +0000

    [CodeFactor] Apply fixes to commit 50b4b30

--- db/migrate/20201002122517_remove_spina_conferences_conference_page_parts.rb
+++ db/migrate/20201002122517_remove_spina_conferences_conference_page_parts.rb
@@ -4,9 +4,9 @@ class RemoveSpinaConferencesConferencePageParts < ActiveRecord::Migration[6.0]
   def change
     drop_table :spina_conferences_conference_page_parts do |t|
       t.references :conference_page, foreign_key: { to_table: :spina_pages, on_delete: :cascade },
-                   index: { name: 'index_spina_conferences_parts_on_page_id' }
+                                     index: { name: 'index_spina_conferences_parts_on_page_id' }
       t.references :conference_page_partable, polymorphic: true,
-                   index: { name: 'index_spina_conferences_parts_on_partable_type_and_partable_id' }
+                                              index: { name: 'index_spina_conferences_parts_on_partable_type_and_partable_id' }
 
       t.timestamps
     end
