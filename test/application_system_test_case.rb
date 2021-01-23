4b7348280bce657f06f1a6edf75b2febe39e1952
Justin Malčić <j.malcic@me.com>
2021-01-23 13:26:34 +0000

    Require Selenium if necessary

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,6 +2,11 @@
 
 require 'test_helper'
 
+# Needed because Selenium not required when running via Rake test
+unless self.class.const_defined? :Selenium
+  require 'selenium-webdriver'
+end
+
 ActiveSupport.on_load :action_dispatch_system_test_case do
   ActionDispatch::SystemTesting::Server.silence_puma = true
 end
