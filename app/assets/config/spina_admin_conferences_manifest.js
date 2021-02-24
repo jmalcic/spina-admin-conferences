3c33aa430431a32a638d0f43dc019cd645e58a67
Justin Malčić <j.malcic@me.com>
2021-02-24 14:48:12 +0000

    Fix manifest

--- app/assets/config/spina_admin_conferences_manifest.js
+++ app/assets/config/spina_admin_conferences_manifest.js
@@ -1,5 +1,5 @@
 //= link spina/trix.js
 //= link spina/trix.css
-//= link spina/admin/conferences/application.css
 //= link spina_manifest.js
+//= link spina/admin/conferences/application.css
 //= link_tree ../javascripts
